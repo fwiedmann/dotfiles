f39dbf0ce4120110ad5fd5fce8b35b8a38e1c8f8
Felix Wiedmann <felix.wiedmann@de.markant.com>
2022-08-04 09:15:29 +0200

    feat(plugin): add barbar

--- nvim/lua/fwiedmann/packer.lua
+++ nvim/lua/fwiedmann/packer.lua
@@ -51,4 +51,8 @@ return require('packer').startup(function(use)
   use { "williamboman/nvim-lsp-installer", commit = "e9f13d7acaa60aff91c58b923002228668c8c9e6" } -- simple to use language server installer
   use { "jose-elias-alvarez/null-ls.nvim", commit = "ff40739e5be6581899b43385997e39eecdbf9465" } -- for formatters and linters
   use { "RRethy/vim-illuminate", commit = "c82e6d04f27a41d7fdcad9be0bce5bb59fcb78e5" }
+  use {
+  'romgrk/barbar.nvim',
+  requires = {'kyazdani42/nvim-web-devicons'}
+  }
 end)
