2d698e32cb32f50d5a082fd9fe22758bb3f25954
fwiedmann <privat@felixwiedmann.de>
2022-08-21 00:05:29 +0200

    feat: add autocmd

---
+++ nvim/lua/fwiedmann/autocmd.lua
@@ -0,0 +1,15 @@
+local api = vim.api
+
+-- Highlight on yank
+local yankGrp = api.nvim_create_augroup("YankHighlight", { clear = true })
+api.nvim_create_autocmd("TextYankPost", {
+  command = "silent! lua vim.highlight.on_yank()",
+  group = yankGrp,
+})
+
+-- lsp-saga
+local sagaGrp = api.nvim_create_augroup("LspSaga", {})
+api.nvim_create_autocmd("CursorHold", {
+  command = "Lspsaga signature_help",
+  group = sagaGrp,
+})
