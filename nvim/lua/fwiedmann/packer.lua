27afeb0e27c081d82b6942f921c929c5c4921c6d
fwiedmann <privat@felixwiedmann.de>
2022-09-11 14:00:06 +0200

    feat: add diffview

--- nvim/lua/fwiedmann/packer.lua
+++ nvim/lua/fwiedmann/packer.lua
@@ -72,6 +72,6 @@ return require('packer').startup(function(use)
 
   use 'laytan/cloak.nvim'
   use { "catppuccin/nvim", as = "catppuccin" }
-
+  use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' }
 
 end)
