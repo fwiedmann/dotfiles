54de6dd0496d3e97abe42163cb1b58d79ad71d14
Felix Wiedmann <felix.wiedmann@de.markant.com>
2022-11-10 11:58:12 +0100

    ref: which_key show spellings

--- nvim/lua/fwiedmann/which-key.lua
+++ nvim/lua/fwiedmann/which-key.lua
@@ -4,4 +4,10 @@ if not status_ok then
 end
 
 which_key.setup({
+  plugins = {
+    spelling = {
+      enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+      suggestions = 20, -- how many suggestions should be shown in the list?
+    },
+  }
 })
